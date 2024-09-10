4d5262dbcced106f68f95f707b1d621543bdfc91
EWART-boop <johnewartdelacruz@gmail.com>
2024-09-10 12:41:01 +0800

    variables.js.

---
+++ variables.js
@@ -0,0 +1,11 @@
+const name = "John Ewart Dela Cruz";
+console.log('My name is', name);
+
+const whatDoYouWannaBecomeInYourLife = 'Programmer';
+console.log('I wanna become a', whatDoYouWannaBecomeInYourLife);
+
+const gender = "Male";
+console.log('My gender is', gender);
+
+const twitterHandle = '@J_Ewart_DC';
+console.log('My twitter Handler is', twitterHandle);
